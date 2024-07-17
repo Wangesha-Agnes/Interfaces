c3333cc1bb29add76200ce3296be4cdbadbe10a4
Wangesha-Agnes <wangeshaagnes@gmail.com>
2024-07-17 18:28:41 +0300

    added changes

--- src/Main.kt
+++ src/Main.kt
@@ -6,6 +6,13 @@ val bus = Bus()
     bus.bookTicket("person")
     bus.loadGoods()
 
+    val samsung= SamsungTv()
+    val hisense = HisenseTV()
+    samsung.switchOn()
+    hisense.switchOn()
+    hisense.adjustVolume(6)
+    samsung.adjustVolume(8)
+
 
 
 }
@@ -30,3 +37,60 @@ class Bus(): Transport(){
     }
 }
 
+
+interface Television{
+    fun adjustVolume(volume: Int)
+
+    fun switchChannel(channel: String)
+
+    fun switchOn()
+
+    fun swithOff()
+}
+
+class SamsungTv(): Television{
+    var myVolume = 0
+    var myChannel = "KBC"
+    override fun adjustVolume(volume: Int) {
+       var myVolume = 0
+
+    }
+
+    override fun switchChannel(channel: String) {
+        var myChannel = channel
+    }
+
+    override fun switchOn() {
+        println("Switching on")
+    }
+
+    override fun swithOff() {
+        println("Switching off")
+    }
+
+}
+
+class HisenseTV(): Television{
+    var hsVol = -5
+    var hsChannel = ""
+    var on:Boolean = false
+
+    override fun adjustVolume(volume: Int) {
+      hsVol = 0
+        hsVol = (volume*2)/2
+    }
+
+    override fun switchChannel(channel: String) {
+    hsChannel = channel
+
+    }
+
+    override fun switchOn() {
+        println("Switching on")
+    }
+
+    override fun swithOff() {
+        println("Switching off")
+    }
+
+}
